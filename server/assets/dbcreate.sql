92ec7ebc334620b1dea30bda9f22563aee32971a
carlos <carlos.urena@student.fairfield.edu>
2020-02-26 14:16:32 -0500

    BACK TO POSTGRES LOL + update requests

--- server/assets/dbcreate.sql
+++ server/assets/dbcreate.sql
@@ -1,3 +1,92 @@
+--POSTGRESQL
+CREATE table people(
+   	person_id SERIAL PRIMARY KEY,
+    first_name varchar(50) not null,
+    middle_name varchar(50),
+    last_name varchar(50) not null,
+    nickname varchar(50),
+    gender varchar(1) not null,
+    birthdate date,
+    email varchar(50),
+    address varchar(50),
+    city varchar(50),
+    state varchar(2),
+    phone varchar(15),
+    member_type varchar(50),
+    allergies varchar(255),
+    grade int,
+    user_id int,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+
+CREATE table users(
+   	user_id SERIAL PRIMARY KEY,
+    email varchar(50) unique not null,
+    username varchar(50) not null,
+    password varchar(50) not null,
+    auth_status varchar(50) not null,
+    oauth_provider varchar(50),
+    photourl varchar(255),
+    person_id int not null REFERENCES people(person_id),
+    last_login_date timestamp,
+    active boolean NOT NULL,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+
+
+CREATE table mdv_events(
+   	event_id SERIAL PRIMARY KEY,
+    event_name varchar(50) not null,
+    recurring boolean,
+    frequency varchar(50) not null,
+    day_of_week varchar(50) not null,
+    time_of_day timestamp,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+
+CREATE table mdv_locations(
+   	location_id SERIAL PRIMARY KEY,
+    location_name varchar(50) not null,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+
+CREATE table mdv_sheets(
+   	sheet_id SERIAL PRIMARY KEY,
+    event_id int not null REFERENCES mdv_events(event_id),
+    location_id int not null REFERENCES mdv_locations(location_id),
+    sheet_date timestamp not null,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+
+CREATE table mdv_attendee_entries(
+   	entry_id SERIAL PRIMARY KEY,
+    sheet_id int not null REFERENCES mdv_sheets(sheet_id),
+    entry_type varchar(50) not null,
+    entry_date timestamp not null,
+    person_id int REFERENCES people(person_id),
+    headcount int,
+    created_on timestamp not null,
+    created_by varchar(50) not null,
+    updated_on timestamp not null,
+    updated_by varchar(50) not null
+    );
+    
+--MYSQL
 CREATE table people(
    	person_id int AUTO_INCREMENT PRIMARY KEY,
     first_name varchar(50) not null,
--- server/index.js
+++ server/index.js
@@ -1,237 +1,300 @@
+//API Code for use with PostgreSQL DB if needed. Might switch back.
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-var mysql = require('mysql')
+const pgp = require('pg-promise')();
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('./server/application.properties');
 
 
-const db = mysql.createConnection({
-  host: '107.180.4.132', // 'localhost' is the default;
+const cn = {
+  host: 'localhost', // 'localhost' is the default;
+  port: 5433, // 5432 is the default;
   database: 'manantial',
-  user: 'carlosurenajr',
-  password: 'vH5a67dcafag77D'
-});
+  user: 'postgres',
+  password: 'urenafamily'
+};
 
-db.connect()
-
-db.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-
+var db = pgp(cn)
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 
-
-
-
-// //test req
-// app.get('/api/greeting', (req, res) => {
-//   const name = req.query.name || 'World';
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-// });
+//test req
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
 
 //---------------- USER ACCOUNTS ----------------------//
 const getAllUsers = properties.get('user.get.all')
 const getUser = properties.get('user.get.one')
-const createUser = properties.get('user.create.one.mysql')
+const createUser = properties.get('user.create.one')
+const updateUser = properties.get('user.update.field.one')
 const deleteUser = properties.get('user.delete.one')
 
 
 //GET: all registered user accounts
-app.get('/api/accounts', (req, res) => {
-  db.query(getAllUsers, (err, result, fields) => {
-    if (err) throw err;
-    console.log(result);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(result));
-  })
+app.get('/api/users', (req, res) => {
+  db.any(getAllUsers)
+    .then(function (data) {
+      // success;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      // error;
+      console.log('error retrieving users', error)
+    });
+});
+
+//GET: SINGLE USER ACCT
+app.get('/api/users/:uid', (req, res) => {
+  db.oneOrNone(getUser, req.params.uid)
+    .then(function (data) {
+      // success;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      // error;
+      console.log('error retrieving users', error)
+    });
 });
 
 
-// //GET: SINGLE USER ACCT
-// app.get('/api/accounts/:accountId', (req, res) => {
-//   db.oneOrNone(getAccount, req.params.accountId)
-//     .then(function (data) {
-//       // success;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify(data));
-//     })
-//     .catch(function (error) {
-//       // error;
-//       console.log('error retrieving accounts', error)
-//     });
-// });
-
 //POST: create new user account
-app.post('/api/accounts', (req, res) => {
-  const data = {email: 'carlos@testEmail.com', username: 'curena', password: 'test123', auth_status : 'pending', oauth_provider: null, photourl: null, person_id : 1, active: true, created_by: 'SYSTEM', updated_by: 'SYSTEM'}
-  db.query(createUser, data, (err, results) =>{
-    if(err) throw err;
-    //console.log(results.user_id); // print new account id;
-    console.log('done')
-  })
- 
+app.post('/api/users', (req, res) => {
+  db.one(createUser, ['carlos@testEmail.com', 'curena', 'test123', 'pending', null, null, 2, true, 'SYSTEM', 'SYSTEM'])
+    .then(data => {
+      console.log(data.user_id); // print new account id;
+      res.send(data.user_id);
+
+    })
+    .catch(error => {
+      if (error.code == 23505) {
+        //UNIQUE CONSTRAINT VIOLATION
+        console.log('USER ALREADY EXISTS');
+        res.status(400).send(JSON.stringify({ 'response': 'USER ALREADY EXISTS' }));
+      } else {
+        console.log('ERROR:', error); // print error;
+        res.status(400).send(JSON.stringify({ 'response': 'ERROR' }));
+      }
+    });
+});
+
+//PUT: update User Account -- wip
+app.put('/api/users/:uid', (req, res) => {
+  const requestingUser = 'curena';
+  const data = { user_id: req.params.uid, email: 'carlitosway@gmail.com', username: 'curena', password: '1234', auth_status: 'member', photourl: null, active: true, last_login_date: null, updated_on: new Date(), updated_by: requestingUser };
+  console.log('updating')
+
+  // Setup the query
+  let query = updateUserByID(req.params.uid, data);
+  // Turn req.body into an array of values
+  var colValues = Object.keys(data).map(function (key) {
+    return data[key];
+  });
+
+  db.any(query, colValues)
+    .then(data => {
+      console.log(data[0].user_id); // print id;
+    })
+    .catch(error => {
+
+      console.log('ERROR:', error); // print error;
+
+    });
+  res.send();
+
+});
+
+function updateUserByID(id, cols) {
+  // Setup static beginning of query
+  var query = ['UPDATE users'];
+  query.push('SET');
+
+  // Create another array storing each set command
+  // and assigning a number value for parameterized query
+  var set = [];
+  Object.keys(cols).forEach(function (key, i) {
+    set.push(key + ' = ($' + (i + 1) + ')');
+  });
+  query.push(set.join(', '));
+
+  // Add the WHERE statement to look up by id
+  query.push('WHERE user_id = ' + id + ' RETURNING USER_ID');
+
+  // Return a complete query string
+  return query.join(' ');
+}
+
+
+
+//DELETE: DELETE User
+app.delete('/api/users/:uid', (req, res) => {
+  db.result(deleteUser, req.params.uid)
+    .then(result => {
+      // success;
+      // rowCount = number of rows affected by the query
+      console.log(result.rowCount + " rows deleted."); // print how many records were deleted;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ 'response': result.rowCount + ' rows deleted.' }));
+    })
+    .catch(error => {
+      // error;
+      console.log('error deleting user acct', error)
+    });
+});
+
+
+//---------------- PEOPLE ----------------------//
+const getAllPeople = properties.get('person.get.all')
+const createPerson = properties.get('person.create.one')
+const getPerson = properties.get('person.get.one')
+const deletePerson = properties.get('person.delete.one')
+
+//GET: all registered people
+app.get('/api/people', (req, res) => {
+  db.any(getAllPeople)
+    .then(function (data) {
+      // success;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      // error;
+      console.log('error retrieving accounts', error)
+    });
+});
+
+//GET: SINGLE PERSON
+app.get('/api/people/:personId', (req, res) => {
+  db.oneOrNone(getPerson, req.params.personId)
+    .then(function (data) {
+      // success;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      // error;
+      console.log('error retrieving accounts', error)
+    });
+});
+
+//POST: create new Person
+app.post('/api/people', (req, res) => {
+  db.one(createPerson, ['Carlos', 'E', 'Ureña', 'M', new Date(), 'carlosurenajr@gmail.com', '89 Leonard Dr.', 'Bridgeport', 'CT', '2035227369', 'visitor', null, 10, null, null, 'SYSTEM', 'SYSTEM'])
+    .then(data => {
+      console.log("person created, id: " + data.person_id); // print new account id;
+    })
+    .catch(error => {
+      if (error.code == 23505) {
+        //UNIQUE CONSTRAINT VIOLATION
+        console.log('ACCOUNT ALREADY EXISTS');
+      } else {
+        console.log('ERROR:', error); // print error;
+      }
+    });
   res.send();
 });
 
-// //POST: create new user account
-// app.post('/api/accounts', (req, res) => {
-//   db.one(createAccount, ['carlos@testEmail.com', 'curena', 'test123', 'pending', null, null, 4, true, 'SYSTEM', 'SYSTEM'])
-//     .then(data => {
-//       console.log(data.account_id); // print new account id;
-//     })
-//     .catch(error => {
-//       if (error.code == 23505) {
-//         //UNIQUE CONSTRAINT VIOLATION
-//         console.log('ACCOUNT ALREADY EXISTS');
-//       } else {
-//         console.log('ERROR:', error); // print error;
-//       }
-//     });
-//   res.send();
-// });
+//PUT: update PERSON -- wip
+app.put('/api/people/:pid', (req, res) => {
+  const requestingUser = 'curena';
+  const data = { first_name: 'Carlos', middle_name: 'Eliezer', last_name: 'Urena', updated_on: new Date(), updated_by: requestingUser };
+  console.log('updating')
 
-// //PUT: update User Account -- wip
-// app.put('/api/accounts', (req, res) => {
-//   db.one(createAccount, ['carlos@testEmail.com', 'curena', 'test123', 'pending', null, null, 4, 'SYSTEM', 'SYSTEM'])
-//     .then(data => {
-//       console.log(data.account_id); // print new account id;
-//     })
-//     .catch(error => {
-//       if (error.code == 23505) {
-//         //UNIQUE CONSTRAINT VIOLATION
-//         console.log('ACCOUNT ALREADY EXISTS');
-//       } else {
-//         console.log('ERROR:', error); // print error;
-//       }
-//     });
-//   res.send();
-// });
+  // Setup the query
+  let query = updatePersonByID(req.params.pid, data);
+  console.log(query)
+  // Turn req.body into an array of values
+  var colValues = Object.keys(data).map(function (key) {
+    return data[key];
+  });
 
+  db.any(query, colValues)
+    .then(data => {
+      console.log(data[0].person_id); // print id;
+    })
+    .catch(error => {
 
+      console.log('ERROR:', error); // print error;
 
-// //DELETE: DELETE Account
-// app.delete('/api/accounts/:accountId', (req, res) => {
-//   db.result(deleteAccount, req.params.accountId)
-//     .then(result => {
-//       // success;
-//       // rowCount = number of rows affected by the query
-//       console.log(result.rowCount + " rows deleted."); // print how many records were deleted;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify( {'response' : result.rowCount +' rows deleted.'}));
-//     })
-//     .catch(error => {
-//       // error;
-//       console.log('error deleting person', error)
-//     });
-// });
+    });
+  res.send();
 
+});
 
-// //---------------- PEOPLE ----------------------//
-// const getAllPeople = properties.get('person.get.all')
-// const createPerson = properties.get('person.create.one')
-// const getPerson = properties.get('person.get.one')
-// const deletePerson = properties.get('person.delete.one')
+function updatePersonByID(id, cols) {
+  // Setup static beginning of query
+  var query = ['UPDATE people'];
+  query.push('SET');
 
-// //GET: all registered people
-// app.get('/api/people', (req, res) => {
-//   db.any(getAllPeople)
-//     .then(function (data) {
-//       // success;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify(data));
-//     })
-//     .catch(function (error) {
-//       // error;
-//       console.log('error retrieving accounts', error)
-//     });
-// });
+  // Create another array storing each set command
+  // and assigning a number value for parameterized query
+  var set = [];
+  Object.keys(cols).forEach(function (key, i) {
+    set.push(key + ' = ($' + (i + 1) + ')');
+  });
+  query.push(set.join(', '));
 
-// //GET: SINGLE PERSON
-// app.get('/api/people/:personId', (req, res) => {
-//   db.oneOrNone(getPerson, req.params.personId)
-//     .then(function (data) {
-//       // success;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify(data));
-//     })
-//     .catch(function (error) {
-//       // error;
-//       console.log('error retrieving accounts', error)
-//     });
-// });
+  // Add the WHERE statement to look up by id
+  query.push('WHERE person_id = ' + id + ' RETURNING PERSON_ID');
 
-// //POST: create new Person
-// app.post('/api/people', (req, res) => {
-//   db.one(createPerson, ['Carlos', 'E', 'Ureña', 'M', new Date(), 'carlosurenajr@gmail.com', '89 Leonard Dr.', 'Bridgeport', 'CT', '2035227369', 'visitor', null, 10, null, null, 'SYSTEM', 'SYSTEM'])
-//     .then(data => {
-//       console.log("person created, id: " + data.person_id); // print new account id;
-//     })
-//     .catch(error => {
-//       if (error.code == 23505) {
-//         //UNIQUE CONSTRAINT VIOLATION
-//         console.log('ACCOUNT ALREADY EXISTS');
-//       } else {
-//         console.log('ERROR:', error); // print error;
-//       }
-//     });
-//   res.send();
-// });
+  // Return a complete query string
+  return query.join(' ');
+}
 
-// //DELETE: DELETE Person
-// app.delete('/api/people/:personId', (req, res) => {
-//   db.result(deletePerson, req.params.personId)
-//     .then(result => {
-//       // success;
-//       // rowCount = number of rows affected by the query
-//       console.log(result.rowCount + " rows deleted."); // print how many records were deleted;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify( {'response' : result.rowCount +' rows deleted.'}));
-//     })
-//     .catch(error => {
-//       // error;
-//       console.log('error deleting person', error)
-//     });
-// });
+//DELETE: DELETE Person
+app.delete('/api/people/:personId', (req, res) => {
+  db.result(deletePerson, req.params.personId)
+    .then(result => {
+      // success;
+      // rowCount = number of rows affected by the query
+      console.log(result.rowCount + " rows deleted."); // print how many records were deleted;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ 'response': result.rowCount + ' rows deleted.' }));
+    })
+    .catch(error => {
+      // error;
+      console.log('error deleting person', error)
+    });
+});
 
-// //---------------- AUTHENTICATION/AUTHORIZATION ----------------------//
-// const checkCredentials = properties.get('auth.login')
+//---------------- AUTHENTICATION/AUTHORIZATION ----------------------//
+const checkCredentials = properties.get('auth.login')
 
-// //GET: VERIFY USER CREDENTIALS
-// app.post('/api/auth/login', (req, res) => {
-//   console.log('checking')
-//   console.log(req)
+//GET: VERIFY USER CREDENTIALS
+app.post('/api/auth/login', (req, res) => {
+  console.log('checking')
+  console.log(req)
 
-//   db.one(checkCredentials, ['curena', 'tesdt123'])
-//     .then(function (data) {
-//       // success;
-//       console.log('credential check returned: ' +  data)
-//       res.setHeader('Content-Type', 'application/json');
-//       res.send(JSON.stringify(data)); 
-//     })
-//     .catch(function (error) {
-//       // error;
-//       console.log('error verifying accounts', error)
-//     });
-// });
+  db.one(checkCredentials, ['curena', 'tesdt123'])
+    .then(function (data) {
+      // success;
+      console.log('credential check returned: ' + data)
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      // error;
+      console.log('error verifying accounts', error)
+    });
+});
 
-// //------------------------------------------------------------------------//
-// //---------------------------------CHECK-INS------------------------------//
-// //------------------------------------------------------------------------//
+//------------------------------------------------------------------------//
+//---------------------------------CHECK-INS------------------------------//
+//------------------------------------------------------------------------//
 
-// //---------------- EVENTS ----------------------//
+//---------------- EVENTS ----------------------//
 
-// //---------------- LOCATIONS ----------------------//
+//---------------- LOCATIONS ----------------------//
 
-// //---------------- SHEETS ----------------------//
+//---------------- SHEETS ----------------------//
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')

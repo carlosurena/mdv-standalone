4af5f4df49d0af771e0b98be1f98619544d900a2
Carlos <carlos.urenar@student.fairfield.edu>
2020-03-26 12:50:37 -0400

    center header

--- src/components/checkins/checkinHeader.js
+++ src/components/checkins/checkinHeader.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Row, Col, Layout } from 'antd';
+import React from "react";
+import { Row, Col, Layout } from "antd";
 
 const { Header } = Layout;
 
 function CheckinHeader(props) {
   return (
     <Header className="checkin-header">
-      <Row align="middle" gutter={[8]} justify="space-between">
+      <Row align="middle" gutter={[8]} justify="center">
         <Col xs={24} md={16}>
-          bro
+          <h1>Event Type</h1>
         </Col>
       </Row>
     </Header>
